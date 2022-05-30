3351737507cd950239c0a23ab580f56e946126f6
Blngoe <boleng0909@gmail.com>
2022-05-30 15:54:06 +0800

    cxcvxc

--- CodeCamp_excercise/Arithmetic_Formatter/arithmetic_arranger.py
+++ CodeCamp_excercise/Arithmetic_Formatter/arithmetic_arranger.py
@@ -1,4 +1,10 @@
 def arithmetic_arranger(problems):
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
+    arranged_problems = ""
 
-    return arranged_problems
+    for index, value in enumerate(problems):
+        operation = value.split(" ")
+
+    return arranged_problems 
