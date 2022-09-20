958f9e96a80e0ad1fd9799df17314dcee329db4e
Blngoe <boleng0909@gmail.com>
2022-09-20 23:25:50 +0800

    sept 20

--- LinkedIn/Ex_Files_Python_Object_Programming/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/instance_start.py
+++ LinkedIn/Ex_Files_Python_Object_Programming/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/instance_start.py
@@ -1,8 +1,8 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using instance methods and attributes
 
-
 from re import S
+from select import select
 
 
 class Book:
@@ -14,21 +14,32 @@ class Book:
         self.author = author
         self.page = page
         self.price = price
+        self.__secret = "This is a secret attribute"
         
     # TODO: create instance methods
     def getPrice(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
         return self.price
+    
+    def setDiscount(self, amount):
+        self._discount = amount
 
 
 # TODO: create some book instances
 b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
 
-# TODO: print the price of book1
-print(b1.price)
+# # TODO: print the price of book1
+# print(b1.getPrice())
 
 
-# TODO: try setting the discount
-
+# # TODO: try setting the discount
+# print("as",b2.getPrice())
+# b2.setDiscount(0.25)
+# print(b2.getPrice())
 
 # TODO: properties with double underscores are hidden by the interpreter
+print(b2._Book__secret)
