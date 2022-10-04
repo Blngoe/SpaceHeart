79b8250fca380ecd1312819fc26b8662b61981d3
Blngoe <boleng0909@gmail.com>
2022-10-04 23:20:40 +0800

    write

--- LinkedIn/Ex_Files_Learning_Python/Ch2 - Basics/helloworld_start.py
+++ LinkedIn/Ex_Files_Learning_Python/Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,10 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("Hello World")
+    name = input("what is your Name: ")
+    print("Nice to meet you", name)
 
+if __name__ == "__main__":
+    main()
