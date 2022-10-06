480052b20d61c53d8e144d29b520140a65fef5b2
Blngoe <boleng0909@gmail.com>
2022-10-06 22:15:43 +0800

    commit new

--- LinkedIn/Ex_Files_Learning_Python/Ch4 - Dates and Times/formatting_start.py
+++ LinkedIn/Ex_Files_Learning_Python/Ch4 - Dates and Times/formatting_start.py
@@ -4,25 +4,37 @@
 #
 
 
-from datetime import datetime
+from datetime import datetime, date
 
 def main():
     # Times and dates can be formatted using a set of predefined string
     # control codes 
+    now = datetime.now()
 
     
     #### Date Formatting ####
-    
+    #  13-Mar-2020 16:42:58
     # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
-
-
+    print(now.strftime("The current year is: %Y" ))
+    print(now.strftime("%a, %d %b %y"))
+    print(now.strftime("%d-%b-%Y %H:%M:%S"))
     # %c - locale's date and time, %x - locale's date, %X - locale's time
-
+    print(now.strftime("Local date and time %c"))
+    print(now.strftime("Local date %x"))
+    print(now.strftime("Local time %X"))
 
     #### Time Formatting ####
     
     # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
-  
+    print(now.strftime("Current time %I:%M:%S %p"))
+    print(now.strftime("Current time %H:%M"))
 
+
+    today=date.today()
+    days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    print("Tomorrow will be "+days[(today.weekday()+1)%7])
+    today=date.today()
+    days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    print("Tomorrow will be "+days[today.weekday()+1])
 if __name__ == "__main__":
     main()
--- LinkedIn/Ex_Files_Learning_Python/Ch5 - Internet Data/htmlparsing_start.py
+++ LinkedIn/Ex_Files_Learning_Python/Ch5 - Internet Data/htmlparsing_start.py
@@ -5,15 +5,36 @@
 
 from html.parser import HTMLParser
 
+paragraph = 0
 class MyHTMLParser(HTMLParser):
     def handle_comment(self, data):
-        pass
+        print("Encountered a comment", data)
+        
+        pos = self.getpos()
+        print("At line",pos[0], "position", pos[1])
 
     def handle_starttag(self, tag, attrs):
-        pass
+        print("Encountered a start tag", tag)
+        
+        pos = self.getpos()
+        print("At line",pos[0], "position", pos[1])
+        
+        global paragraph
+        if tag == "p":
+            paragraph+=1
+
+        if len(attrs):
+            print("attributes")
+            for a in attrs:
+                print("\t",a[0], "=", a[1])
+
 
     def handle_data(self, data):
-        pass
+        if data.isspace():
+            pass
+        print("Encountered a text", data)
+        pos = self.getpos()
+        print("At line",pos[0], "position", pos[1])
 
 def main():
     # instantiate the parser and feed it some HTML
@@ -23,7 +44,7 @@ def main():
     if f.mode == "r":
         contents = f.read() # read the entire file
         parser.feed(contents)    
-
+    print("paragraph tag", paragraph)
 if __name__ == "__main__":
     main()
   
