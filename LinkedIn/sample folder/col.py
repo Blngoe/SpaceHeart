31e3f045ca9bb28fc45bbc11db93376e5de5e986
Blngoe <boleng0909@gmail.com>
2022-12-12 18:33:14 +0800

    asd1

--- LinkedIn/sample folder/col.py
+++ LinkedIn/sample folder/col.py
@@ -1,1 +1,11 @@
-asd
+from itertools import combinations
+N = int (input())
+#print (N)
+L = input().split()
+# print(L)
+
+K = int(input())
+
+C = list(combinations(L,K))
+F = filter (lambda c : 'b' in c, C)
+print('{0: .3}'.format(len(list(F))/len(C)))
