39080ce84bed416f33363dc386bea97cef5cf27a
Blngoe <103173298+Blngoe@users.noreply.github.com>
2023-08-18 15:32:04 +0800

    dasdsa

---
+++ LinkedIn/Ex_Files_Learning_Python_3_Standard_Library_2022/Exercise Files/Ch01/01_01/01_01_Start.py
@@ -0,0 +1,23 @@
+# Python Logical Operators: And, Or, Not:
+
+# What is a Boolean?
+isRaining = True
+isSunn = False
+# Logical Operators -> Special Operators for Booleans
+
+# AND
+# true and true --> true
+# false and true --> false
+# true and false --> false
+# false and false --> false
+if isRaining and isSunn:
+    print("rainbow")
+# OR
+# true and true --> true
+# false and true --> true
+# true and false --> true
+# false and false --> false
+
+# NOT
+# true --> false
+# false --> true
